7c1cf14f4757f5e0bea42eece38e0fcc89d928b0
Ron Alleva <ron@konciergemd.com>
2012-04-25 13:04:08 -0400

    Change the test to be more like our tests, with a set up and tear down.  Same result

--- test/models/SimpleModelTest.java
+++ test/models/SimpleModelTest.java
@@ -1,16 +1,42 @@
 package models;
 
-import org.junit.Test;
-import static org.fest.assertions.Assertions.assertThat;
+import org.junit.*;
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.start;
+import static play.test.Helpers.stop;
+
+import play.test.FakeApplication;
+
 
 
 public class SimpleModelTest {
 
+    private static FakeApplication application;
+
+    @BeforeClass
+    public static void oneTimeSetup() {
+        application = fakeApplication();
+        start(application);
+    }
+
+    @AfterClass
+    public static void oneTimeTearDown() {
+        stop(application);
+    }
 
     @Test
     public void test() {
         int i = 1 + 1;
 
         assertThat(i).isEqualTo(2);
+
+        SimpleModel model = new SimpleModel();
+        model.name = "Hi";
+        model.save();
+
+        SimpleModel retrievedModel = SimpleModel.findById(model.id);
+        assertThat(retrievedModel.name).isEqualTo("Hi");
     }
 }
